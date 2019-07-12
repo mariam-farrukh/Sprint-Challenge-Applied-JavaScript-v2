17976169f6e33efbc762ad6835f39d94dc973486
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-12 10:50:28 -0500

    Created axios portion of tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,26 @@
 // Step 2: Create Tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// Using axios send a GET 
+// request to the address: https://lambda-times-backend.herokuapp.com/topics
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then(res =>{
+    console.log('it works', res.data);
+})
+.catch(error => {
+    console.log('uh oh', error);
+})
+
+// const tabsTopics = document.querySelector('.topics');
+
+// tabsTopics.appendChild(Tabs());
+
+// function Tabs(){
+
+// }
